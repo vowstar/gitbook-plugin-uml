620dfffd1cf045f8c062e28fa88362c64e787f1b
Huang Rui <vowstar@gmail.com>
2016-10-19 13:08:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+# gitbook-plugin-uml
+
+[![NPM](https://nodei.co/npm/gitbook-plugin-uml.png)](https://nodei.co/npm/gitbook-plugin-uml/)
+
+[PlantUml](http://www.plantuml.com/) Plugin for [GitBook](https://github.com/GitbookIO/gitbook)
+
+This is a sample plugin for GitBook and is specially adapted for GitBook from [PlantUML](http://www.plantuml.com/index.html). Gitbook PlantUml plugin is used to select from markdown uml and converting it into a picture format svg.
+
+## Installation
+
+Gitbook PlantUml plugin can be installed from NPM using:
+
+```bash
+$ npm install gitbook-plugin-uml
+```
+
+## Features
+
+* Support HTML, PDF, EPUB output(make sure your gitbook support SVG)
+* Support ```flow code block quote
+* Multi code style support
+
+
+### Beautiful UML
+
+![](./images/uml.png)
+
+**Image uml.**
+
+
+## How to use it
+
+To include a PlantUML diagram, just wrap your definition in a "uml" code block. For example:
+
+*Text format uml:*
+
+<pre><code>```uml
+@startuml
+
+	Class Stage
+	Class Timeout {
+		+constructor:function(cfg)
+		+timeout:function(ctx)
+		+overdue:function(ctx)
+		+stage: Stage
+	}
+ 	Stage &lt;|-- Timeout
+
+@enduml
+```
+</code></pre>
+
+## Configuration
+
+book.json add the uml options
+
+Configure plugin in `book.json`.
+
+```
+{
+    "plugins": ["uml"]
+}
+```
+
+## Build and serve
+
+This plugin only works in your local machine. You need to play with local `gitbook` (command-line tool) to pre-compile all uml images.
+
+```$ gitbook serve yourbook```
+
+or 
+
+```$ gitbook build yourbook```
+
+## Additional requirements:
+
+ - Create a directory */assets/images/uml* in the root of your project.
+ - [Install PlantUML.](http://www.plantuml.com/download.html) (Download plantuml.jar to root path)
+
+For Mac OS X users. Install *graphviz* package.
+
+```$ brew install graphviz```
+
+For Linux users, Install *graphviz* package.
+
+```$ sudo apt install graphviz```
+
+```$ sudo yum install graphviz```
+
+This plugin original from [lyhcode/gitbook-plugin-plantuml](https://github.com/lyhcode/gitbook-plugin-plantuml)
+
+
---
+++ index.js
@@ -0,0 +1,110 @@
+var spawn = require('child_process').spawn;
+var fs = require('fs-extra');
+var crypto = require('crypto');
+var plantuml = require('node-plantuml');
+var Q = require('q');
+
+function processBlock(blk) {
+    var deferred = Q.defer();
+
+    var book = this;
+    var code = blk.body;
+    var config = book.config.get('pluginsConfig.uml', {});
+
+    if (blk.kwargs['config']) {
+        config = blk.kwargs['config'];
+    }
+
+    var gen = plantuml.generate(code, config);
+
+    var chunks = [];
+    gen.out.on('data', function(chunk) {
+        chunks.push(chunk)
+    })
+    gen.out.on('end', function() {
+        var buffer = Buffer.concat(chunks)
+        var format = "png";
+        if (config)
+            format = config.format;
+
+        var assetPath = './assets/images/uml/';
+        var filePath = assetPath + crypto.createHash('sha1').update(code).digest('hex') + '.' + format;
+        fs.mkdirs(assetPath, function(err) {
+            if (err)
+                console.error(err);
+        })
+
+        fs.writeFile(filePath, buffer, (err) => {
+            if (err)
+                console.error(err);
+        });
+
+        var result = "<img src=" + filePath + ">";
+        deferred.resolve(result);
+    })
+    return deferred.promise;
+}
+
+module.exports = {
+    blocks: {
+        uml: {
+            process: processBlock
+        }
+    },
+    hooks: {
+        // For all the hooks, this represent the current generator
+        // [init", "finish", "finish:before", "page", "page:before"] are working.
+        // page:* are marked as deprecated because it's better if plugins start using blocks instead. 
+        // But page and page:before will probably stay at the end (useful in some cases).
+
+        // This is called before the book is generated
+        "init": function() {
+            if (!Object.keys(this.book.config.get('pluginsConfig.uml', {})).length) {
+                this.book.config.set('pluginsConfig.uml', {
+                    format: 'png'
+                });
+            }
+        },
+
+        // This is called after the book generation
+        "finish": function() {
+            // Done
+        },
+
+        // Before the end of book generation
+        "finish:before": function() {
+            // Copy images to output folder every time
+            var book = this;
+            var output = book.output;
+            var rootPath = output.root();
+            if (rootPath) {
+                fs.mkdirs(rootPath + '/assets/images/uml/');
+                fs.copy('./assets/images/uml/', rootPath + '/assets/images/uml/', {
+                    clobber: true
+                }, function(err) {
+                    if (err)
+                        console.error(err)
+                })
+            }
+        },
+
+        // The following hooks are called for each page of the book
+        // and can be used to change page content (html, data or markdown)
+
+
+        // Before parsing documents
+        "page:before": function(page) {
+            // Get all code texts
+            umls = page.content.match(/^```uml((.*\n)+?)?```$/igm);
+            // Begin replace
+            if (umls instanceof Array) {
+                for (var i = 0, len = umls.length; i < len; i++) {
+                    page.content = page.content.replace(
+                        umls[i],
+                        umls[i].replace(/^```uml/, '{% uml %}').replace(/```$/, '{% enduml %}'));
+                }
+            }
+            return page;
+        }
+    }
+};
