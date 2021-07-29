c7bcc6b09fc67843f89042878c95eb782b5b3b80
Huang Rui <vowstar@gmail.com>
2021-07-30 00:17:16 +0800

    Add a new test case to test include uml
    Signed-off-by: Huang Rui <vowstar@gmail.com>

--- test/index.js
+++ test/index.js
@@ -8,6 +8,8 @@ var TEST_CONTENT_PNG = '<p><img src="assets/images/uml/39e6cfed5bc41c359e02bb07b
 var TEST_CONTENT_SVG = '<p><img src="assets/images/uml/39e6cfed5bc41c359e02bb07bcfcbcb365bd61eb.svg"></p>';
 var TEST_SRCFILE_PNG = '<p><img src="assets/images/uml/5e39e6908033eaefb9e853d01eb7f2462a7ea69c.png"></p>';
 var TEST_SRCFILE_SVG = '<p><img src="assets/images/uml/5e39e6908033eaefb9e853d01eb7f2462a7ea69c.svg"></p>';
+var TEST_INCFILE_PNG = '<p><img src="assets/images/uml/b25548fd30f84bae07fd6b18e02a5015fe3531d2.png"></p>';
+var TEST_INCFILE_SVG = '<p><img src="assets/images/uml/b25548fd30f84bae07fd6b18e02a5015fe3531d2.svg"></p>';
 
 describe('uml', function() {
     it('should correctly replace by ```uml``` tag', function() {
@@ -93,6 +95,23 @@ describe('uml', function() {
             .then(function(result) {
                 var passPng = (result[0].content == TEST_SRCFILE_PNG);
                 var passSvg = (result[0].content == TEST_SRCFILE_SVG);
+                assert(passPng || passSvg, true);
+            });
+    });
+    it('should correctly use external plantuml file specified by ```puml @startuml !include ... @enduml tag', function() {
+        return tester.builder()
+            .withContent('\n```puml\n@startuml\n!include test/test.plantuml\n@enduml\n```')
+            .withLocalPlugin(path.join(__dirname, '..'))
+            .withLocalDir(path.join(__dirname, '..', 'test'))
+            .withBookJson({
+                plugins: ['uml']
+            })
+            .create()
+            .then(function(result) {
+                console.log(TEST_SRCFILE_SVG);
+                console.log(TEST_SRCFILE_PNG);
+                var passPng = (result[0].content == TEST_INCFILE_PNG);
+                var passSvg = (result[0].content == TEST_INCFILE_SVG);
                 assert(passPng || passSvg, true);
             });
     });
